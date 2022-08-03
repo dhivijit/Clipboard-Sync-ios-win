59f58a03b615a31eefd26e9b35394d24eb5cf0cc
Dhivijit Koppuravuri <68179868+dhivijit@users.noreply.github.com>
2022-08-03 10:17:01 +0530

    Update

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 //esversion:6
-
 const express = require("express");
 const app = express();
 const ip = require("ip");
 const bodyParser = require("body-parser");
-
 var ncp = require("node-clipboardy");
 
 app.use(
@@ -13,20 +11,19 @@ app.use(
   })
 );
 
-app
-  .route("/copy")
-  .get((req, res) => {
-    res.send(
-      '<form action="/copy" method="post"><input type="text" name="clipfromuser" id="clipfromuser"><input type="submit" value=""></form>'
-    );
-  })
-  .post((req, res) => {
-    ncp.writeSync(req.body.clipfromuser);
-    console.log("Copied");
-    res.send("Copied");
-  });
+app.post("/ios", (req, res) => {
+  ncp.writeSync(req.body.clipfromuser);
+  console.log("Received");
+  res.send("Sent");
+});
+
+app.get("/windows", (req, res) => {
+  var winclip = ncp.readSync();
+  res.send(winclip);
+  console.log("Sent");
+});
 
 app.listen(4541, function () {
   console.log("Server is running on port 4541");
-  console.log("Your local IP address is " + ip.address());
+  console.log("Your IP address is " + ip.address());
 });
